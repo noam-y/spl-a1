b0a41a924d93ee88d6314e1fe33c6db371a5876f
Noam <noamj33@gmail.com>
2024-11-27 17:16:23 +0000

    finished building constuctors and fixing basic functions inside facility

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
 using namespace std;
 #include "../include/Facility.h"
+#include "../include/Plan.h"
 
 
-class Facility: public FacilityType{
-    public:
-        Facility(const string &name, const string &settlementName,
+        Facility::Facility(const string &name, const string &settlementName,
         const FacilityCategory category, const int price,
          const int lifeQuality_score, const int economy_score,
           const int environment_score)
           :FacilityType(name, category, price, lifeQuality_score, economy_score, environment_score), // Pass to base class constructor
-          settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS) {};
+          settlementName(settlementName), status(FacilityStatus::UNDER_CONSTRUCTIONS) {
+            timeLeft = price;
+          };
           
-        Facility(const FacilityType &type, const string &settlementName); // TODO
+         Facility::Facility(const FacilityType &type, const string &settlementName):
+         Facility(settlementName,settlementName,type.getCategory(), type.getCost(), type.getLifeQualityScore(),
+         type.getEconomyScore(), type.getEnvironmentScore()){};
 
-        const string &getSettlementName() const{
+
+        const string &Facility::getSettlementName() const{
             return settlementName;
         }
-        const int getTimeLeft() const{
+        const int Facility::getTimeLeft() const{
             return timeLeft;
         }
-        FacilityStatus step(){
-            if (timeLeft > 1){
+        FacilityStatus Facility::step(){
+            if (timeLeft =0 ){
                 setStatus(FacilityStatus::OPERATIONAL);
+                //Plan::addFacility(*this); TODO
             }
             else{
                 timeLeft = timeLeft - 1;
             }
 
-            // TODO  ADD TO FACILITY LIST
+            
         }
-        void setStatus(FacilityStatus status){
+        void Facility::setStatus(FacilityStatus status){
             this->status = status;
         }
-        const FacilityStatus& getStatus() const{
+        const FacilityStatus& Facility::getStatus() const{
             return status;
         }
-        const string toString() const{
+        const string Facility::toString() const{
             cout << "Facility Name: " << getName() << "\n";
             cout << "Settlement Name: " << getSettlementName() << "\n";
         }
 
-    private:
         const string settlementName;
         FacilityStatus status;
         int timeLeft;
-};
