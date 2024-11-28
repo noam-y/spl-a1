15eb87dccb7b31b23a0c21b2d35eed78f8a97bb0
Noam <noamj33@gmail.com>
2024-11-28 08:35:29 +0000

    finished constructor and basic functions- step, printStatus, getters inside plan

--- src/plan.cpp
+++ src/plan.cpp
@@ -3,16 +3,22 @@
 #include "../include/Plan.h"
 #include "../include/Settlement.h"
 #include "../include/SelectionPolicy.h"
+#include "../include/Facility.h"
+#include <iostream>
 using std::vector;
+using namespace std;
 
 
 Plan::Plan(const int planId, const Settlement &settlement, SelectionPolicy *selectionPolicy, const vector<FacilityType> &facilityOptions):
-plan_id(planId), settlement(settlement), selectionPolicy(selectionPolicy), status(), facilities(), underConstruction(), facilityOptions(facilityOptions){
+plan_id(planId), settlement(settlement), selectionPolicy(selectionPolicy), facilityOptions(facilityOptions){
+     status = PlanStatus::AVALIABLE;
+    vector<Facility*> facilities; //TODO FIX
+    vector<Facility*> underConstruction;
     life_quality_score = 0;
     economy_score = 0;
     environment_score = 0;
 } 
-
+//TODO: COPY CONSTRUCTOR destructor, copy constructor, copy assignment opreator HALF RULE OF FIVE- EXPLAINED IN FORUM
 const int Plan::getlifeQualityScore() const{
     return life_quality_score;
 }
@@ -22,9 +28,30 @@ const int Plan::getEconomyScore() const{
 const int Plan::getEnvironmentScore() const{
     return environment_score;
 }
-void Plan::setSelectionPolicy(SelectionPolicy *selectionPolicy){ this->selectionPolicy = selectionPolicy;};
-        // void step(); //TODO
-        // void printStatus();
+void Plan::setSelectionPolicy(SelectionPolicy *selectionPolicy){ this->selectionPolicy = selectionPolicy;};// TODO destruct prev selection policy
+
+
+void Plan::step(){
+    for (Facility* facility : underConstruction) {
+        FacilityStatus f1 = facility->step();
+        if (f1 == FacilityStatus::OPERATIONAL){
+            addFacility(facility);
+        }
+    }
+}
+
+
+ //TODO
+void Plan::printStatus(){
+    string s;
+    if (status == PlanStatus:: AVALIABLE){
+        s = "AVALIABLE";
+    }
+    else{
+        s = "UNDER CONSTRUCTION";
+    }
+    cout << "status: " + s << endl;
+}
         // const vector<Facility*> &getFacilities() const;
         // void addFacility(Facility* facility);
         // const string toString() const;
